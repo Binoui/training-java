e1ba4a1baa28c94df98103d386c84a140cc817e6
bhoux <bhoux@excilys.com>
2018-03-12 10:33:18 +0100

    init gitignore

---
+++ src/com/excilys/formation/cdb/model/Company.java
@@ -0,0 +1,44 @@
+package com.excilys.formation.cdb.model;
+
+import java.sql.Date;
+
+public class Company {
+	private int id;
+	private String name;
+	private Date introduced;
+	private Date discontinued;
+	
+	public Company() {}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getIntroduced() {
+		return introduced;
+	}
+
+	public void setIntroduced(Date introduced) {
+		this.introduced = introduced;
+	}
+
+	public Date getDiscontinued() {
+		return discontinued;
+	}
+
+	public void setDiscontinued(Date discontinued) {
+		this.discontinued = discontinued;
+	}
+}
---
+++ src/com/excilys/formation/cdb/model/Computer.java
@@ -0,0 +1,25 @@
+package com.excilys.formation.cdb.model;
+
+public class Computer {
+	
+	private int id;
+	private String name;
+
+	public Computer() {}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
