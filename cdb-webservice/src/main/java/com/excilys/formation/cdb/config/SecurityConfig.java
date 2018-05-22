7b619fed5e5be26d11111bd3890d5ca0cfe1eb36
bhoux <bhoux@excilys.com>
2018-05-22 16:44:38 +0200

    [WIP]

--- cdb-webservice/src/main/java/com/excilys/formation/cdb/config/SecurityConfig.java
+++ cdb-webservice/src/main/java/com/excilys/formation/cdb/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.excilys.formation.cdb.config;
 
-import lombok.experimental.FieldDefaults;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,85 +19,71 @@ import org.springframework.security.web.util.matcher.OrRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 
 import static java.util.Objects.requireNonNull;
-import static lombok.AccessLevel.PRIVATE;
 import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled=true)
-@FieldDefaults(level = PRIVATE, makeFinal = true)
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-  private static final RequestMatcher PUBLIC_URLS = new OrRequestMatcher(
-    new AntPathRequestMatcher("/public/**")
-  );
-  private static final RequestMatcher PROTECTED_URLS = new NegatedRequestMatcher(PUBLIC_URLS);
+    private static final RequestMatcher PUBLIC_URLS = new OrRequestMatcher(new AntPathRequestMatcher("/public/**"));
+    private static final RequestMatcher PROTECTED_URLS = new NegatedRequestMatcher(PUBLIC_URLS);
 
-  TokenAuthenticationProvider provider;
+    private final TokenAuthenticationProvider provider;
 
-  SecurityConfig(final TokenAuthenticationProvider provider) {
-    super();
-    this.provider = requireNonNull(provider);
-  }
+    public SecurityConfig(final TokenAuthenticationProvider provider) {
+        super();
+        this.provider = requireNonNull(provider);
+    }
 
-  @Override
-  protected void configure(final AuthenticationManagerBuilder auth) {
-    auth.authenticationProvider(provider);
-  }
+    @Override
+    protected void configure(final AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(provider);
+    }
 
-  @Override
-  public void configure(final WebSecurity web) {
-    web.ignoring().requestMatchers(PUBLIC_URLS);
-  }
+    @Override
+    public void configure(final WebSecurity web) {
+        web.ignoring().requestMatchers(PUBLIC_URLS);
+    }
 
-  @Override
-  protected void configure(final HttpSecurity http) throws Exception {
-    http
-      .sessionManagement()
-      .sessionCreationPolicy(STATELESS)
-      .and()
-      .exceptionHandling()
-      // this entry point handles when you request a protected page and you are not yet
-      // authenticated
-      .defaultAuthenticationEntryPointFor(forbiddenEntryPoint(), PROTECTED_URLS)
-      .and()
-      .authenticationProvider(provider)
-      .addFilterBefore(restAuthenticationFilter(), AnonymousAuthenticationFilter.class)
-      .authorizeRequests()
-      .anyRequest()
-      .authenticated()
-      .and()
-      .csrf().disable()
-      .formLogin().disable()
-      .httpBasic().disable()
-      .logout().disable();
-  }
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
+        http.sessionManagement().sessionCreationPolicy(STATELESS).and().exceptionHandling()
+                // this entry point handles when you request a protected page and you are not
+                // yet
+                // authenticated
+                .defaultAuthenticationEntryPointFor(forbiddenEntryPoint(), PROTECTED_URLS).and()
+                .authenticationProvider(provider)
+                .addFilterBefore(restAuthenticationFilter(), AnonymousAuthenticationFilter.class).authorizeRequests()
+                .anyRequest().authenticated().and().csrf().disable().formLogin().disable().httpBasic().disable()
+                .logout().disable();
+    }
 
-  @Bean
-  TokenAuthenticationFilter restAuthenticationFilter() throws Exception {
-    final TokenAuthenticationFilter filter = new TokenAuthenticationFilter(PROTECTED_URLS);
-    filter.setAuthenticationManager(authenticationManager());
-    filter.setAuthenticationSuccessHandler(successHandler());
-    return filter;
-  }
+    @Bean
+    TokenAuthenticationFilter restAuthenticationFilter() throws Exception {
+        final TokenAuthenticationFilter filter = new TokenAuthenticationFilter(PROTECTED_URLS);
+        filter.setAuthenticationManager(authenticationManager());
+        filter.setAuthenticationSuccessHandler(successHandler());
+        return filter;
+    }
 
-  @Bean
-  SimpleUrlAuthenticationSuccessHandler successHandler() {
-    final SimpleUrlAuthenticationSuccessHandler successHandler = new SimpleUrlAuthenticationSuccessHandler();
-    successHandler.setRedirectStrategy(new NoRedirectStrategy());
-    return successHandler;
-  }
+    @Bean
+    SimpleUrlAuthenticationSuccessHandler successHandler() {
+        final SimpleUrlAuthenticationSuccessHandler successHandler = new SimpleUrlAuthenticationSuccessHandler();
+        successHandler.setRedirectStrategy(new NoRedirectStrategy());
+        return successHandler;
+    }
 
-  @Bean
-  FilterRegistrationBean<?> disableAutoRegistration(final TokenAuthenticationFilter filter) {
-    final FilterRegistrationBean registration = new FilterRegistrationBean<>(filter);
-    registration.setEnabled(false);
-    return registration;
-  }
+    @Bean
+    FilterRegistrationBean<?> disableAutoRegistration(final TokenAuthenticationFilter filter) {
+        final FilterRegistrationBean registration = new FilterRegistrationBean<>(filter);
+        registration.setEnabled(false);
+        return registration;
+    }
 
-  @Bean
-  AuthenticationEntryPoint forbiddenEntryPoint() {
-    return new HttpStatusEntryPoint(UNAUTHORIZED);
-  }
+    @Bean
+    AuthenticationEntryPoint forbiddenEntryPoint() {
+        return new HttpStatusEntryPoint(UNAUTHORIZED);
+    }
 
 }
--- cdb-webservice/src/main/java/com/excilys/formation/cdb/config/WebConfig.java
+++ cdb-webservice/src/main/java/com/excilys/formation/cdb/config/WebConfig.java
@@ -1,14 +1,9 @@
 package com.excilys.formation.cdb.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -26,46 +21,5 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
     
-    @Autowired
-    private RestAuthenticationEntryPoint restAuthenticationEntryPoint;
- 
-    @Autowired
-    private MySavedRequestAwareAuthenticationSuccessHandler
-      authenticationSuccessHandler;
- 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth)
-      throws Exception {
-  
-        auth.inMemoryAuthentication()
-          .withUser("temporary").password("temporary").roles("ADMIN")
-          .and()
-          .withUser("user").password("userPass").roles("USER");
-    }
- 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception { 
-        http
-        .csrf().disable()
-        .exceptionHandling()
-        .authenticationEntryPoint(restAuthenticationEntryPoint)
-        .and()
-        .authorizeRequests()
-        .antMatchers("/api/foos").authenticated()
-        .and()
-        .formLogin()
-        .successHandler(authenticationSuccessHandler)
-        .failureHandler(new SimpleUrlAuthenticationFailureHandler())
-        .and()
-        .logout();
-    }
- 
-    @Bean
-    public MySavedRequestAwareAuthenticationSuccessHandler mySuccessHandler(){
-        return new MySavedRequestAwareAuthenticationSuccessHandler();
-    }
-    @Bean
-    public SimpleUrlAuthenticationFailureHandler myFailureHandler(){
-        return new SimpleUrlAuthenticationFailureHandler();
-    } 
+    
 }
